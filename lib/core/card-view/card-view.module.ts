de7abfd12b5658a7519128641478f38a36a3d202
Denys Vuika <denis.vuyka@gmail.com>
2018-01-12 17:06:11 +0000

    update card view after rebase

--- lib/core/card-view/card-view.module.ts
+++ lib/core/card-view/card-view.module.ts
@@ -38,9 +38,6 @@ import { CardViewItemDispatcherComponent } from './components/card-view-item-dis
 import { CardViewMapItemComponent } from './components/card-view-mapitem/card-view-mapitem.component';
 import { CardViewTextItemComponent } from './components/card-view-textitem/card-view-textitem.component';
 
-import { CardItemTypeService } from './services/card-item-types.service';
-import { CardViewUpdateService } from './services/card-view-update.service';
-
 @NgModule({
     imports: [
         CommonModule,
@@ -54,7 +51,7 @@ import { CardViewUpdateService } from './services/card-view-update.service';
         MatIconModule,
         MatButtonModule,
         MatDatetimepickerModule,
-        MatNativeDatetimeModule    
+        MatNativeDatetimeModule
     ],
     declarations: [
         CardViewComponent,
@@ -69,7 +66,7 @@ import { CardViewUpdateService } from './services/card-view-update.service';
         CardViewBoolItemComponent,
         CardViewDateItemComponent,
         CardViewMapItemComponent,
-        CardViewTextItemComponent    
+        CardViewTextItemComponent
     ],
     exports: [
         CardViewComponent,
@@ -77,10 +74,6 @@ import { CardViewUpdateService } from './services/card-view-update.service';
         CardViewDateItemComponent,
         CardViewMapItemComponent,
         CardViewTextItemComponent
-    ],
-    providers: [
-        CardItemTypeService,
-        CardViewUpdateService
     ]
 })
 export class CardViewModule {}
